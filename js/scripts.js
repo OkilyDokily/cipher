097e29fb34c7c40bca2644746dd8332a1d2fc390
me <me@me.me.com>
2020-03-24 15:06:35 -0700

    add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,57 @@
-$(document).ready()
+$(document).ready(function(){
+  
+  
+  
+  
+  var string = prompt("Enter a sentence") 
+  console.log(string);
+  // function capitalizeFirstAndLast(string){
+  //   var capitalizeFirst = string.replace(/^./,string.charAt(0).toUpperCase());
+  //   var capitalizeLast = capitalizeFirst.replace(/.$/,capitalizeFirst.charAt(string.length-1).toUpperCase());
+  //   return capitalizeLast;
+  // }
+
+  // function switchFirstAndLast(string){
+  //   var switchFirst = string.replace(/^./,string.charAt(string.length - 1));
+  //   var switchLast = switchFirst.replace(/.$/,string.charAt(0));
+  //   return switchLast;
+  // }
+
+  function capitalizeFirstAndLast(string){
+    var array = string.match(/^.|.$/g)
+    return (array[0] + array[1]).toUpperCase();; 
+  }
+
+  function switchFirstAndLast(string){
+    var array = capitalizeFirstAndLast(string);
+    return array[1] + array[0];
+  }
+
+  function concatenateSwitch(string){
+    return string + switchFirstAndLast(string);
+  }
+
+  function reverseConcatenate(string){
+    return concatenateSwitch(string).split("").reverse().join("");
+  }
+  
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
